66d1ccbcf29201145ceadacee121343c430c4f0b
dayang4321 <dayang4321@users.noreply.github.com>
2020-08-15 12:05:44 +0100

    implement navbutton animation

--- src/components/Header/Navigation/Navbar/Navbar.js
+++ src/components/Header/Navigation/Navbar/Navbar.js
@@ -4,8 +4,10 @@ import Nav from 'react-bootstrap/Nav'
 import logo from '../../../UI/Logo/logo.svg'
 import NavDropdown from './NavDropdown/NavDropdown'
 import Button from '../../../UI/Button/Button'
+import NavButton from './NavButton/NavButton';
 
 import './Navbar.css'
+
 
 
 const MainNavbar = (props) => {
@@ -19,20 +21,20 @@ const MainNavbar = (props) => {
 
 
     return (
-        <Navbar className={navState + " p-xl-5 p-lg-4 p-md-2 p-sm-3 p-2"} collapseOnSelect expand="md" onToggle={toggleNav} variant="dark">
-            <Navbar.Brand href="#" className="px-xl-5 px-lg-3 px-0"> <img src={logo} className="navbar-logo img-fluid" alt="logo" /></Navbar.Brand>
-                <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse className="px-xl-4 px-lg-3 px-md-0 px-0" id="responsive-navbar-nav">
-                  <Nav className="ml-auto">
-                    <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#about">About BookAha</Nav.Link>
-                    <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#signup">SignUp</Nav.Link>
-                </Nav>
-                <NavDropdown/>
-                  <Nav>
-                    <Button btnText="Download App" clicked={props.openModal}/>
-                  </Nav>
-                </Navbar.Collapse>
-        </Navbar>
+      <Navbar className={navState + " p-xl-5 p-lg-4 p-md-2 p-0"} id="mainNavbar" expanded={navState==="expanded"&&true}  expand="md" onToggle={toggleNav} variant="dark">
+      <Navbar.Brand href="#" className="px-xl-5 px-lg-4 px-0 mx-3 mx-md-0"> <img src={logo} className="navbar-logo img-fluid" alt="logo" /></Navbar.Brand>
+  <NavButton navButtonClass={navState} clicked={toggleNav}/>
+          <Navbar.Collapse className="px-xl-4 px-lg-3 px-md-0 px-0" id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#about">About BookAha</Nav.Link>
+              <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#signup">SignUp</Nav.Link>
+          </Nav>
+          <NavDropdown/>
+            <Nav>
+      <Button btnText="Download App" clicked={props.openModal}/>
+            </Nav>
+          </Navbar.Collapse>
+  </Navbar>
 
     )
 }
