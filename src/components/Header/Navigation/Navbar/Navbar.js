84efe9be7bd861021fd15beb374b1bbb9c6d11ed
dayang4321 <dayang4321@users.noreply.github.com>
2021-08-09 11:33:32 +0100

    add translate animation to navbar

--- src/components/Header/Navigation/Navbar/Navbar.js
+++ src/components/Header/Navigation/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import logo from '../../../UI/Logo/logo.svg'
 import NavDropdown from './NavDropdown/NavDropdown'
 import Button from '../../../UI/Button/Button'
 import NavButton from './NavButton/NavButton';
 import { useTranslation } from "react-i18next";
+import { motion } from 'framer-motion';
 
 
 import './Navbar.css'
+import AnimatedLogo from '../../../UI/Logo/AnimatedLogo';
 
 
 
@@ -24,25 +25,65 @@ const MainNavbar = (props) => {
    return setNavState(navState==="expanded"?"":"expanded")
   }
 
+  const navVariants = {
+    start: {
+      opacity: 0,
+      y: -100,
+    },
+    end: {
+      opacity:100,
+      y: 0,
+      transition: {
+        duration: 1.25,
+        ease: 'easeInOut'
+      }
+    },
+  };
 
 
     return (
-      <Navbar  className={navState + " p-xl-5 p-lg-4 p-md-2 p-0"} id="mainNavbar" fixed="top" expanded={navState==="expanded"&&true}  expand="md" onToggle={toggleNav} variant="dark">
-      <Navbar.Brand href="#" className="px-xl-5 px-lg-4 px-0 mx-3 mx-md-0"> <img src={logo} className="navbar-logo img-fluid" alt="logo" /></Navbar.Brand>
-  <NavButton navButtonClass={navState} clicked={toggleNav}/>
-          <Navbar.Collapse className="px-xl-4 px-lg-3 px-md-0 px-0" id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#about">{t("header.navbar.about")}</Nav.Link>
-              <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#signup">{t("header.navbar.signup")}</Nav.Link>
+      <Navbar
+        as={motion.nav}
+        className={navState + ' p-xl-5 p-lg-4 p-md-2 p-0'}
+        id="mainNavbar"
+        fixed="top"
+        expanded={navState === 'expanded' && true}
+        expand="md"
+        onToggle={toggleNav}
+        variant="dark"
+        variants={navVariants}
+        initial="start"
+        animate="end"
+      >
+        <Navbar.Brand href="#" className="px-xl-5 px-lg-4 px-0 mx-3 mx-md-0">
+          <AnimatedLogo className="navbar-logo img-fluid" alt="logo" />
+        </Navbar.Brand>
+        <NavButton navButtonClass={navState} clicked={toggleNav} />
+        <Navbar.Collapse
+          className="px-xl-4 px-lg-3 px-md-0 px-0"
+          id="responsive-navbar-nav"
+        >
+          <Nav className="ml-auto">
+            <Nav.Link className="px-xl-4 px-lg-3 px-2 py-3 py-md" href="#about">
+              {t('header.navbar.about')}
+            </Nav.Link>
+            <Nav.Link
+              className="px-xl-4 px-lg-3 px-2 py-3 py-md"
+              href="#signup"
+            >
+              {t('header.navbar.signup')}
+            </Nav.Link>
           </Nav>
-          <NavDropdown/>
-            <Nav>
-      <Button btnText={t("header.navbar.download")} clicked={props.openModal}/>
-            </Nav>
-          </Navbar.Collapse>
-  </Navbar>
-
-    )
+          <NavDropdown />
+          <Nav>
+            <Button
+              btnText={t('header.navbar.download')}
+              clicked={props.openModal}
+            />
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
 }
 
 
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { motion } from 'framer-motion';
 
-import './Button.css'
+import './Button.css';
 
 const myButton = (props) => {
-    return (
-        <Button variant="light" type={props.btnType}  onClick={props.clicked?() => props.clicked() : null}  className="white-button b-0 text-dark bg-white border-0 rounded-0"> {props.btnText} </Button>
-    )
-}
+  return (
+    <Button
+      as={motion.button}
+      whileTap={{
+        scale: 0.92,
+      }}
+      whileHover={{
+        scale: 1.04,
+      }}
+      variant="light"
+      type={props.btnType}
+      onClick={props.clicked ? () => props.clicked() : null}
+      className="white-button b-0 text-dark bg-white border-0 rounded-sm"
+    >
+      {' '}
+      {props.btnText}{' '}
+    </Button>
+  );
+};
 
-
-export default myButton
-
-
+export default myButton;
