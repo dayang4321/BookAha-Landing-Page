9786efc5e5ad3faa3dd92c7061f602a8809f5999
dayang4321 <dayang4321@users.noreply.github.com>
2021-08-10 09:54:38 +0100

    adjust footer responsive style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,8 +14,8 @@ const footer = (main) => {
            
             <Register registerParent="footer"/>
             <div className="footer-base">
-                <img src={logo} className="footer-logo mb-lg-5 mb-md-3 mb-2" alt="logo" />
-                <p className="copyright">Copyright &copy; 2020. BookAHA.</p>
+                <img src={logo} className="footer-logo mb-md-3 mb-2" alt="logo" />
+                <p className="copyright">Copyright &copy; {new Date().getFullYear()}. BookAHA.</p>
             </div>
        
         </footer>
