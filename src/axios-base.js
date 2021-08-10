04dfc7b0041291c02c917f364a8da9ea59764cd8
dayang4321 <dayang4321@users.noreply.github.com>
2021-08-10 11:28:26 +0100

    add .env to gitignore

--- src/axios-base.js
+++ src/axios-base.js
@@ -9,32 +9,5 @@ const instance = Axios.create({
       },
 });
 
-instance.interceptors.request.use(req => {
-  //   if (Axios.defaults.headers.common["Authorization"]) return req;
-  //   throw ({message:"the token is not available"});
-  //  },error=>{
-  //   return Promise.reject(error);
-  //  }
-  //console.log(req)
-  return req
-}
-);
-
-export const setAuthToken = token => {
-  if (token) {
-    instance.defaults.headers['Authorization'] = 'Bearer ' + token;
-  //applying token
-    // console.log(instance);
-    // console.log(instance.defaults);
-    // console.log(instance.defaults.headers);
-    // console.log(instance.defaults.headers.common);
-    // console.log(instance.defaults.headers['Authorization']);
-    
-  } else {
-  //deleting the token from header
-  //delete instance.defaults.headers.common['Authorization'];
-  }
- }
-
 
 export default instance
