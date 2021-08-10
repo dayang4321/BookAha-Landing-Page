23d5d73c4b2df38d4622d8535736b842c80aa3f9
dayang4321 <dayang4321@users.noreply.github.com>
2021-08-10 09:28:08 +0100

    add animation to about section

--- src/components/Main/InfoBlock/InfoBlock.js
+++ src/components/Main/InfoBlock/InfoBlock.js
@@ -1,26 +1,79 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import './InfoBlock.css'
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import './InfoBlock.css';
+import { motion } from 'framer-motion';
 
+//Ease
+const transition = {
+  duration: 1,
+ // ease: [0.6, 0.01, -0.05, 0.9],
+  ease: "easeInOut"
+};
+
+const topLeftVariants = {
+  initial: {
+    x: '-2.22rem',
+    y: '-2.5rem',
+    transition: {
+      ...transition,
+    },
+  },
+  hover: {
+    x: 0,
+    y: 0,
+    transition: {
+      ...transition,
+    },
+  },
+};
+
+const topRightVariants = {
+  initial: {
+    x: '2.22rem',
+    y: '-2.5rem',
+    transition: {
+      ...transition,
+    },
+  },
+  hover: {
+    x: 0,
+    y: 0,
+    transition: {
+      ...transition,
+    },
+  },
+};
 
 const infoBlock = (props) => {
-
-    return (
-        <Row className="info-row">
-        <Col xs={12}  md={6} className={"info-text-block " + props.textClass }>
-            <h1 className="info-title mb-0">
-                {props.infoTitle}
-            </h1>
-            <p className="info-text mb-0">{props.infoText}</p>
-        </Col>
-        <Col xs={'auto'}  md={{span: "auto", order: props.imgOrder }}  className={`${"img-block " + props.imgBlockClass} ${props.imgOrder? " mr-auto ":" ml-lg-auto " }`}>
-            <div className="black-bg">
-                <img className={"img-fluid " + props.imgClass} src={require("../../../assets/images/" + props.imgSrc)} alt={props.imgAlt}/>
-            </div>
-        </Col>
+  return (
+    <Row className="info-row d-flex align-items-center">
+      <Col xs={12} md={6} className={'info-text-block ' + props.textClass}>
+        <h1 className="info-title mb-0">{props.infoTitle}</h1>
+        <p className="info-text mb-0">{props.infoText}</p>
+      </Col>
+      <Col
+        xs={'auto'}
+        md={{ span: 'auto', order: props.imgOrder }}
+        className={`${'img-block ' + props.imgBlockClass} ${
+          props.imgOrder ? ' mr-auto ' : ' ml-lg-auto '
+        }`}
+      >
+        <div className={`black-bg ${props.imgClass}`}>
+          <motion.img
+            initial="initial"
+            whileHover="hover"
+            exit="initial"
+            variants={
+              props.imgClass === 'left' ? topLeftVariants : topRightVariants
+            }
+            className={'img-fluid about-img'}
+            src={require('../../../assets/images/' + props.imgSrc)}
+            alt={props.imgAlt}
+          />
+        </div>
+      </Col>
     </Row>
-    )
-  
-}
+  );
+};
 
-export default infoBlock
+export default infoBlock;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -13,11 +13,11 @@ const Main = (props) => {
         <main id="about">
             <Container fluid='lg'>
                 <InfoBlock infoTitle={t("main.block1.title")} textClass="pl-xl-5 pl-lg-4  pl-sm-5 pl-4" infoText={t("main.block1.text")} 
-                  imgBlockClass="mx-auto"  imgSrc="notes.png" imgAlt="notes" imgClass="top-left"/>
+                  imgBlockClass="mx-auto"  imgSrc="notes.png" imgAlt="notes" imgClass="left"/>
                 <InfoBlock infoTitle={t("main.block2.title")} textClass="pl-sm-5 pl-4" infoText={t("main.block2.text")} 
-                  imgBlockClass="mx-auto"  imgSrc="bookClub.png" imgAlt="notes" imgClass="top-right" imgOrder="first" />
+                  imgBlockClass="mx-auto"  imgSrc="bookClub.png" imgAlt="notes" imgClass="right" imgOrder="first" />
                 <InfoBlock infoTitle={t("main.block3.title")} textClass="pl-xl-5 pl-lg-4  pl-sm-5 pl-4" infoText={t("main.block3.text")} 
-                  imgBlockClass="ml-md-n5 pl-md-4 pl-lg mx-auto"  imgSrc="library.png" imgAlt="notes" imgClass="top-right" />
+                  imgBlockClass="ml-md-n5 pl-md-4 pl-lg mx-auto"  imgSrc="library.png" imgAlt="notes" imgClass="right" />
             </Container>
         </main>
     )
